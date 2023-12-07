46dd9871b9d34cc6c2a0ae54a8b8181539de4390
tak5633 <35234375+tak5633@users.noreply.github.com>
2023-12-07 16:57:57 -0500

    main - Feat(Day1): Part 2

--- Day1/day1.go
+++ Day1/day1.go
@@ -42,6 +42,44 @@ func GetTwoDigitNumber(pString string) (int64, error) {
    return (firstDigit*10) + lastDigit, nil
 }
 
+func ConvertTextToDigits(pInput string) string {
+   output := ""
+
+   for inputIdx := 0 ; inputIdx < len(pInput) ; {
+      convertedInput, _ := TryToGetDigit(pInput[inputIdx:])
+
+      // Some numbers share a single character suffix/prefix
+      // To prevent the first number from impacting the second number increment the index by one
+      // Examples:
+      // - twone
+      // - eightwo
+      // - eighthree
+      // - oneight
+      // - threeight
+      // - fiveight
+      // - nineight
+      // - sevenine
+      output += convertedInput
+      inputIdx += 1
+   }
+
+   return output
+}
+
+func TryToGetDigit(pInput string) (string, int) {
+   digitTexts := [9]string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+
+   for digitTextsIdx := 0 ; digitTextsIdx < len(digitTexts) ; digitTextsIdx++ {
+      digitText := digitTexts[digitTextsIdx]
+
+      if len(digitText) <= len(pInput) && digitText == strings.ToLower(pInput[:len(digitText)]) {
+         return strconv.Itoa(digitTextsIdx+1), len(digitText)
+      }
+   }
+
+   return string(pInput[0]), 1
+}
+
 func check(pE error) {
     if pE != nil {
         panic(pE)
@@ -73,6 +111,33 @@ func part1() {
    fmt.Println("Sum:", sum)
 }
 
+func part2() {
+
+   fmt.Println("Part 2")
+
+   input, err := os.ReadFile("./part1Input.txt")
+   check(err)
+
+   inputStr := string(input)
+   inputLines := strings.Split(inputStr, "\n")
+
+   var sum int = 0
+
+   for i := 0 ; i < len(inputLines) ; i++ {
+      inputLine := inputLines[i]
+      convertedInputLine := ConvertTextToDigits(inputLine)
+
+      if len(convertedInputLine) > 0 {
+         twoDigitNumber, err := GetTwoDigitNumber(convertedInputLine)
+         check(err)
+         sum += int(twoDigitNumber)
+      }
+   }
+
+   fmt.Println("Sum:", sum)
+}
+
 func main() {
    part1()
+   part2()
 }
--- Day1/day1_test.go
+++ Day1/day1_test.go
@@ -45,3 +45,136 @@ func TestGetTwoDigitNumber2(t *testing.T) {
       t.Fatal()
    }
 }
+
+func TestConvertTextToDigitsNotADigit(t *testing.T) {
+   newText := ConvertTextToDigits("notAdigit")
+   if newText != "notAdigit" {
+      t.Fatal()
+   }
+}
+
+func TestConvertTextToDigitsThree(t *testing.T) {
+   newText := ConvertTextToDigits("three")
+   if newText != "3hree" {
+      t.Fatal()
+   }
+}
+
+func TestConvertTextToDigitsThreeUppercase(t *testing.T) {
+   newText := ConvertTextToDigits("THREE")
+   if newText != "3HREE" {
+      t.Fatal()
+   }
+}
+
+func TestConvertTextToDigitsThreeMixedCase(t *testing.T) {
+   newText := ConvertTextToDigits("tHrEe")
+   if newText != "3HrEe" {
+      t.Fatal()
+   }
+}
+
+func TestConvertTextToDigitsThreeLeadingChars(t *testing.T) {
+   newText := ConvertTextToDigits("asdfTHREE")
+   if newText != "asdf3HREE" {
+      t.Fatal()
+   }
+}
+
+func TestConvertTextToDigitsThreeTrailingChars(t *testing.T) {
+   newText := ConvertTextToDigits("THREEasdf")
+   if newText != "3HREEasdf" {
+      t.Fatal()
+   }
+}
+
+func TestConvertTextToDigitsThreeLeadingTrailingChars(t *testing.T) {
+   newText := ConvertTextToDigits("asdfTHREEfdsa")
+   if newText != "asdf3HREEfdsa" {
+      t.Fatal()
+   }
+}
+
+func TestConvertTextToDigitsThreeLeadingDigit(t *testing.T) {
+   newText := ConvertTextToDigits("1THREE")
+   if newText != "13HREE" {
+      t.Fatal()
+   }
+}
+
+func TestConvertTextToDigitsThreeTrailingDigit(t *testing.T) {
+   newText := ConvertTextToDigits("THREE1")
+   if newText != "3HREE1" {
+      t.Fatal()
+   }
+}
+
+func TestConvertTextToDigitsThreeLeadingTrailingDigits(t *testing.T) {
+   newText := ConvertTextToDigits("1THREE2")
+   if newText != "13HREE2" {
+      t.Fatal()
+   }
+}
+
+func TestConvertTextToDigitsMultiple(t *testing.T) {
+   newText := ConvertTextToDigits("asdfnine1THREE2eightfdsa")
+   if newText != "asdf9ine13HREE28ightfdsa" {
+      t.Fatal()
+   }
+}
+
+func TestConvertTextToDigitsAocExample1(t *testing.T) {
+   newText := ConvertTextToDigits("two1nine")
+   twoDigitNumber, err := GetTwoDigitNumber(newText)
+   if twoDigitNumber != 29 || err != nil {
+      t.Fatal()
+   }
+}
+
+func TestConvertTextToDigitsAocExample2(t *testing.T) {
+   newText := ConvertTextToDigits("eightwothree")
+   twoDigitNumber, err := GetTwoDigitNumber(newText)
+   if twoDigitNumber != 83 || err != nil {
+      t.Fatal()
+   }
+}
+
+func TestConvertTextToDigitsAocExample3(t *testing.T) {
+   newText := ConvertTextToDigits("abcone2threexyz")
+   twoDigitNumber, err := GetTwoDigitNumber(newText)
+   if twoDigitNumber != 13 || err != nil {
+      t.Fatal()
+   }
+}
+
+func TestConvertTextToDigitsAocExample4(t *testing.T) {
+   newText := ConvertTextToDigits("xtwone3four")
+   twoDigitNumber, err := GetTwoDigitNumber(newText)
+   if twoDigitNumber != 24 || err != nil {
+      t.Fatal()
+   }
+}
+
+func TestConvertTextToDigitsAocExample5(t *testing.T) {
+   newText := ConvertTextToDigits("4nineeightseven2")
+   twoDigitNumber, err := GetTwoDigitNumber(newText)
+   if twoDigitNumber != 42 || err != nil {
+      t.Fatal()
+   }
+}
+
+func TestConvertTextToDigitsAocExample6(t *testing.T) {
+   newText := ConvertTextToDigits("zoneight234")
+   twoDigitNumber, err := GetTwoDigitNumber(newText)
+   if twoDigitNumber != 14 || err != nil {
+      t.Fatal()
+   }
+}
+
+func TestConvertTextToDigitsAocExample7(t *testing.T) {
+   newText := ConvertTextToDigits("7pqrstsixteen")
+   twoDigitNumber, err := GetTwoDigitNumber(newText)
+   if twoDigitNumber != 76 || err != nil {
+      t.Fatal()
+   }
+}
