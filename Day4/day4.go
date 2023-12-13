536c6385d18e92b2a4b176cd189ca9aac36b76d3
tak5633 <35234375+tak5633@users.noreply.github.com>
2023-12-12 23:39:18 -0500

    main - Feat(Day4): Part 2

--- Day4/day4.go
+++ Day4/day4.go
@@ -1,15 +1,25 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"math"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
 
+type Card struct {
+   mWinningNumbers []int
+   mPlayerNumbers []int
+   mNumMatchingNumbers int
+   mNumCopies int
+}
+
 func main() {
    Part1()
+   Part2()
 }
 
 func Part1() {
@@ -18,12 +28,33 @@ func Part1() {
    input, err := os.ReadFile("./input.txt")
    check(err)
 
-   inputStr := string(input)
+   inputStr := strings.TrimSpace(string(input))
    inputLines := strings.Split(inputStr, "\n")
 
    sumOfCards := SumOfCards(inputLines)
 
    fmt.Println("Sum of Cards:", sumOfCards)
+}
+
+func Part2() {
+   fmt.Println("Part 2")
+
+   input, err := os.ReadFile("./input.txt")
+   check(err)
+
+   inputStr := strings.TrimSpace(string(input))
+   inputLines := strings.Split(inputStr, "\n")
+
+   cards := ParseOriginalCards(inputLines)
+   copies := DistributeCopies(cards)
+
+   numCards := 0
+
+   for _, card := range copies {
+      numCards += card.mNumCopies
+   }
+
+   fmt.Println("Number of Cards:", numCards)
 }
 
 func check(pE error) {
@@ -37,27 +68,81 @@ func SumOfCards(pLines []string) int {
    sum := 0
 
    for _, line := range pLines {
-      if len(line) > 0 {
-         numMatchingNumbers := 0
-         winningNumbers, playerNumbers := ParseLine(line)
+      _, winningNumbers, playerNumbers, err := ParseLine(line)
 
-         for _, playerNumber := range playerNumbers {
-            if HasNumber(winningNumbers, playerNumber) {
-               numMatchingNumbers++
-            }
-         }
-
-         points := int(math.Pow(2.0, float64(numMatchingNumbers-1)))
-         sum += points
+      if err != nil {
+         continue
       }
+
+      numMatchingNumbers := GetNumMatchingNumbers(winningNumbers, playerNumbers)
+      points := int(math.Pow(2.0, float64(numMatchingNumbers-1)))
+      sum += points
    }
 
    return sum
 }
 
-func ParseLine(pLine string) ([]int, []int) {
+func ParseOriginalCards(pLines []string) map[int]Card {
+
+   cards := map[int]Card{}
+
+   for _, line := range pLines {
+      cardNumber, winningNumbers, playerNumbers, err := ParseLine(line)
+
+      if err != nil {
+         continue
+      }
+
+      card := Card{}
+      card.mWinningNumbers = winningNumbers
+      card.mPlayerNumbers = playerNumbers
+      card.mNumMatchingNumbers = GetNumMatchingNumbers(winningNumbers, playerNumbers)
+      card.mNumCopies = 1
+
+      cards[cardNumber] = card
+   }
+
+   return cards
+}
+
+func DistributeCopies(pCards map[int]Card) []Card {
+
+   copies := GetSortedCards(pCards)
+
+   for i, card := range copies {
+      for j := 0; j < card.mNumMatchingNumbers; j++ {
+         for k := 0; k < card.mNumCopies; k++ {
+            copyCardIdx := i+j+1
+
+            if copyCardIdx < len(copies) {
+               copies[copyCardIdx].mNumCopies++
+            }
+         }
+      }
+   }
+
+   return copies
+}
+
+func ParseLine(pLine string) (int, []int, []int, error) {
    cardSplit := strings.SplitN(pLine, ":", 2)
+
+   if len(cardSplit) != 2 {
+      return -1, nil, nil, errors.New("ParseLine: Failed to split the card number")
+   }
+
+   cardNumSplit := strings.Split(cardSplit[0], " ")
+   cardNumber, err := strconv.ParseInt(cardNumSplit[len(cardNumSplit)-1], 10, 0)
+
+   if err != nil {
+      return -1, nil, nil, err
+   }
+
    numbersSplit := strings.SplitN(cardSplit[1], "|", 2)
+
+   if len(numbersSplit) != 2 {
+      return -1, nil, nil, errors.New("ParseLine: Failed to split the numbers")
+   }
 
    winningNumberStrs := numbersSplit[0]
    winningNumbers := ParseNumbers(winningNumberStrs)
@@ -65,7 +150,7 @@ func ParseLine(pLine string) ([]int, []int) {
    playerNumberStrs := numbersSplit[1]
    playerNumbers := ParseNumbers(playerNumberStrs)
 
-   return winningNumbers, playerNumbers
+   return int(cardNumber), winningNumbers, playerNumbers, nil
 }
 
 func ParseNumbers(pInput string) []int {
@@ -84,6 +169,18 @@ func ParseNumbers(pInput string) []int {
    return numbers
 }
 
+func GetNumMatchingNumbers(pWinningNumbers []int, pPlayerNumbers []int) int {
+   numMatchingNumbers := 0
+
+   for _, playerNumber := range pPlayerNumbers {
+      if HasNumber(pWinningNumbers, playerNumber) {
+         numMatchingNumbers++
+      }
+   }
+
+   return numMatchingNumbers
+}
+
 func HasNumber(pSlice []int, pTest int) bool {
    for _, element := range pSlice {
       if pTest == element {
@@ -92,4 +189,22 @@ func HasNumber(pSlice []int, pTest int) bool {
    }
 
    return false
+}
+
+func GetSortedCards(pCards map[int]Card) []Card {
+   var cardNumbers []int
+
+   for cardNumber, _ := range pCards {
+      cardNumbers = append(cardNumbers, cardNumber)
+   }
+
+   sort.Ints(cardNumbers)
+
+   var sortedCards []Card
+
+   for _, cardNumber := range cardNumbers {
+      sortedCards = append(sortedCards, pCards[cardNumber])
+   }
+
+   return sortedCards
 }
--- Day4/day4_test.go
+++ Day4/day4_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 )
 
@@ -19,7 +18,11 @@ func Test_ParseNumbers(t *testing.T) {
 
 func Test_ParseLine(t *testing.T) {
    line := "Card   1: 17 15  5 | 13 92 16  5 87 78 15 94 21"
-   winningNumbers, playerNumbers := ParseLine(line)
+   cardNum, winningNumbers, playerNumbers, err := ParseLine(line)
+
+   if err != nil || cardNum != 1 {
+      t.Fatal()
+   }
 
    if len(winningNumbers) != 3 &&
       winningNumbers[0] != 17 && winningNumbers[1] != 15 && winningNumbers[2] != 5 {
@@ -59,7 +62,30 @@ func Test_SumOfCards_Example(t *testing.T) {
    sumOfCards := SumOfCards(lines)
 
    if sumOfCards != 13 {
-      fmt.Println(sumOfCards)
+      t.Fatal()
+   }
+}
+
+func Test_DistributeCopies_Example(t *testing.T) {
+   lines := []string{
+      "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
+      "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
+      "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
+      "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
+      "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
+      "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11",
+   }
+
+   cards := ParseOriginalCards(lines)
+   copies := DistributeCopies(cards)
+
+   numCards := 0
+
+   for _, card := range copies {
+      numCards += card.mNumCopies
+   }
+
+   if numCards != 30 {
       t.Fatal()
    }
 }
