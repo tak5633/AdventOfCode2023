d29c3e7e3f7ba551f571f7a8572259e14a929d18
tak5633 <35234375+tak5633@users.noreply.github.com>
2025-06-17 00:24:45 -0400

    main - Style(Day13): Replaced Tabs with Spaces

--- Day13/day13.go
+++ Day13/day13.go
@@ -21,17 +21,17 @@ func part1() {
    log.Println("Part 1")
 
    inputLines := ReadInput()
-	patterns := ParsePatterns(inputLines)
+   patterns := ParsePatterns(inputLines)
 
-	log.Println("Num Patterns:", len(patterns))
+   log.Println("Num Patterns:", len(patterns))
 
-	requiredNumSmudges := 0
-	patternSummaryValue := GetSummaryValue(patterns, requiredNumSmudges)
-	log.Println("Pattern Summary Value:", patternSummaryValue)
+   requiredNumSmudges := 0
+   patternSummaryValue := GetSummaryValue(patterns, requiredNumSmudges)
+   log.Println("Pattern Summary Value:", patternSummaryValue)
 
-	requiredNumSmudges2 := 1
-	patternSummaryValue2 := GetSummaryValue(patterns, requiredNumSmudges2)
-	log.Println("Pattern Summary Value 2:", patternSummaryValue2)
+   requiredNumSmudges2 := 1
+   patternSummaryValue2 := GetSummaryValue(patterns, requiredNumSmudges2)
+   log.Println("Pattern Summary Value 2:", patternSummaryValue2)
 }
 
 //--------------------------------------------------------------------------------------------------
@@ -62,22 +62,22 @@ func check(pE error) {
 //
 //--------------------------------------------------------------------------------------------------
 func ParsePatterns(pInputs []string) [][]string {
-	var patterns [][]string
+   var patterns [][]string
 
-	var pattern []string
+   var pattern []string
 
-	for _, input := range pInputs {
-	   if len(input) == 0 {
-			patterns = append(patterns, pattern)
-			pattern = make([]string, 0)
-	   } else {
-			pattern = append(pattern, input)
-		}
-	}
+   for _, input := range pInputs {
+      if len(input) == 0 {
+         patterns = append(patterns, pattern)
+         pattern = make([]string, 0)
+      } else {
+         pattern = append(pattern, input)
+      }
+   }
 
-	patterns = append(patterns, pattern)
+   patterns = append(patterns, pattern)
 
-	return patterns
+   return patterns
 }
 
 //--------------------------------------------------------------------------------------------------
@@ -85,27 +85,27 @@ func ParsePatterns(pInputs []string) [][]string {
 //--------------------------------------------------------------------------------------------------
 func GetSummaryValue(pPatterns [][]string, pRequiredNumSmudges int) int {
 
-	patternSummaryValue := 0
+   patternSummaryValue := 0
 
-	for i, pattern := range pPatterns {
-		log.Println("i:", i)
-		rowReflectionIndex, rowReflectionWidth := FindRowReflectionIndex(pattern, pRequiredNumSmudges)
-		colReflectionIndex, colReflectionWidth := FindColReflectionIndex(pattern, pRequiredNumSmudges)
+   for i, pattern := range pPatterns {
+      log.Println("i:", i)
+      rowReflectionIndex, rowReflectionWidth := FindRowReflectionIndex(pattern, pRequiredNumSmudges)
+      colReflectionIndex, colReflectionWidth := FindColReflectionIndex(pattern, pRequiredNumSmudges)
 
-		if rowReflectionIndex != -1 {
-			log.Println("Row:", rowReflectionIndex, "Width:", rowReflectionWidth)
-			patternSummaryValue += (100 * (rowReflectionIndex + 1))
-		}
+      if rowReflectionIndex != -1 {
+         log.Println("Row:", rowReflectionIndex, "Width:", rowReflectionWidth)
+         patternSummaryValue += (100 * (rowReflectionIndex + 1))
+      }
 
-		if colReflectionIndex != -1 {
-			log.Println("Col:", colReflectionIndex, "Width:", colReflectionWidth)
-			patternSummaryValue += (colReflectionIndex + 1)
-		}
+      if colReflectionIndex != -1 {
+         log.Println("Col:", colReflectionIndex, "Width:", colReflectionWidth)
+         patternSummaryValue += (colReflectionIndex + 1)
+      }
 
-		log.Println("Current Pattern Summary Value:", patternSummaryValue)
-	}
+      log.Println("Current Pattern Summary Value:", patternSummaryValue)
+   }
 
-	return patternSummaryValue
+   return patternSummaryValue
 }
 
 //--------------------------------------------------------------------------------------------------
@@ -113,9 +113,9 @@ func GetSummaryValue(pPatterns [][]string, pRequiredNumSmudges int) int {
 //--------------------------------------------------------------------------------------------------
 func FindColReflectionIndex(pPattern []string, pRequiredNumSmudges int) (int, int) {
 
-	transposedPattern := Transpose(pPattern)
+   transposedPattern := Transpose(pPattern)
 
-	return FindRowReflectionIndex(transposedPattern, pRequiredNumSmudges)
+   return FindRowReflectionIndex(transposedPattern, pRequiredNumSmudges)
 }
 
 //--------------------------------------------------------------------------------------------------
@@ -123,27 +123,27 @@ func FindColReflectionIndex(pPattern []string, pRequiredNumSmudges int) (int, in
 //--------------------------------------------------------------------------------------------------
 func FindRowReflectionIndex(pPattern []string, pRequiredNumSmudges int) (int, int) {
 
-	reflectionIndex := -1
-	reflectionWidth := -1
+   reflectionIndex := -1
+   reflectionWidth := -1
 
-	for row := range pPattern {
+   for row := range pPattern {
 
-		upperReflectionWidth := row + 1
-		lowerReflectionWidth := len(pPattern) - row - 1
-		testReflectionWidth := int(math.Min(float64(upperReflectionWidth), float64(lowerReflectionWidth)))
+      upperReflectionWidth := row + 1
+      lowerReflectionWidth := len(pPattern) - row - 1
+      testReflectionWidth := int(math.Min(float64(upperReflectionWidth), float64(lowerReflectionWidth)))
 
-		col := 0
-		numSmudges := 0
+      col := 0
+      numSmudges := 0
 
-		rowReflectionIndex, rowReflectionWidth := FindRowReflectionIndexConstrained(pPattern, row, col, testReflectionWidth, numSmudges, pRequiredNumSmudges)
+      rowReflectionIndex, rowReflectionWidth := FindRowReflectionIndexConstrained(pPattern, row, col, testReflectionWidth, numSmudges, pRequiredNumSmudges)
 
-		if rowReflectionWidth != -1 && rowReflectionWidth >= reflectionWidth {
-			reflectionIndex = rowReflectionIndex
-			reflectionWidth = rowReflectionWidth
-		}
-	}
+      if rowReflectionWidth != -1 && rowReflectionWidth >= reflectionWidth {
+         reflectionIndex = rowReflectionIndex
+         reflectionWidth = rowReflectionWidth
+      }
+   }
 
-	return reflectionIndex, reflectionWidth
+   return reflectionIndex, reflectionWidth
 }
 
 //--------------------------------------------------------------------------------------------------
@@ -151,37 +151,37 @@ func FindRowReflectionIndex(pPattern []string, pRequiredNumSmudges int) (int, in
 //--------------------------------------------------------------------------------------------------
 func FindRowReflectionIndexConstrained(pPattern []string, pRow int, pCol int, pReflectionWidth int, pNumSmudges int, pRequiredNumSmudges int) (int, int) {
 
-	if pReflectionWidth == 0 || pNumSmudges > pRequiredNumSmudges {
-		return -1, -1
-	}
+   if pReflectionWidth == 0 || pNumSmudges > pRequiredNumSmudges {
+      return -1, -1
+   }
 
-	numCols := len(pPattern[0])
+   numCols := len(pPattern[0])
 
-	if pCol >= numCols {
-		if pNumSmudges != pRequiredNumSmudges {
-			return -1, -1
-		}
+   if pCol >= numCols {
+      if pNumSmudges != pRequiredNumSmudges {
+         return -1, -1
+      }
 
-		return pRow, pReflectionWidth
-	}
+      return pRow, pReflectionWidth
+   }
 
-	numSmudges := pNumSmudges
+   numSmudges := pNumSmudges
 
-	for rowOffset := range pReflectionWidth {
+   for rowOffset := range pReflectionWidth {
 
-		upperRow := pRow - rowOffset
-		lowerRow := (pRow + 1) + rowOffset
+      upperRow := pRow - rowOffset
+      lowerRow := (pRow + 1) + rowOffset
 
-		if pPattern[upperRow][pCol] != pPattern[lowerRow][pCol] {
-			numSmudges += 1
-		}
+      if pPattern[upperRow][pCol] != pPattern[lowerRow][pCol] {
+         numSmudges += 1
+      }
 
-		if numSmudges > pRequiredNumSmudges {
-			return -1, -1
-		}
-	}
+      if numSmudges > pRequiredNumSmudges {
+         return -1, -1
+      }
+   }
 
-	return FindRowReflectionIndexConstrained(pPattern, pRow, pCol+1, pReflectionWidth, numSmudges, pRequiredNumSmudges)
+   return FindRowReflectionIndexConstrained(pPattern, pRow, pCol+1, pReflectionWidth, numSmudges, pRequiredNumSmudges)
 }
 
 //--------------------------------------------------------------------------------------------------
@@ -189,24 +189,24 @@ func FindRowReflectionIndexConstrained(pPattern []string, pRow int, pCol int, pR
 //--------------------------------------------------------------------------------------------------
 func Transpose(pPattern []string) []string {
 
-	numRows := len(pPattern)
-	numCols := len(pPattern[0])
+   numRows := len(pPattern)
+   numCols := len(pPattern[0])
 
-	transposedRowPlaceholder := strings.Repeat(" ", numRows)
+   transposedRowPlaceholder := strings.Repeat(" ", numRows)
 
-	var transposedPattern []string
+   var transposedPattern []string
 
-	for range numCols {
-	   transposedPattern = append(transposedPattern, transposedRowPlaceholder)
-	}
+   for range numCols {
+      transposedPattern = append(transposedPattern, transposedRowPlaceholder)
+   }
 
-	for row := range numCols {
-	   for col := range numRows {
-			newRunes := []rune(transposedPattern[row])
-			newRunes[col] = rune(pPattern[col][row])
-	      transposedPattern[row] = string(newRunes)
-	   }
-	}
+   for row := range numCols {
+      for col := range numRows {
+         newRunes := []rune(transposedPattern[row])
+         newRunes[col] = rune(pPattern[col][row])
+         transposedPattern[row] = string(newRunes)
+      }
+   }
 
-	return transposedPattern
+   return transposedPattern
 }
--- Day13/day13_test.go
+++ Day13/day13_test.go
@@ -12,15 +12,15 @@ func Test_FindColReflectionIndex(t *testing.T) {
 
    var pattern []string
 
-	pattern = append(pattern, "#.##..##.")
-	pattern = append(pattern, "..#.##.#.")
-	pattern = append(pattern, "##......#")
-	pattern = append(pattern, "##......#")
-	pattern = append(pattern, "..#.##.#.")
-	pattern = append(pattern, "..##..##.")
-	pattern = append(pattern, "#.#.##.#.")
+   pattern = append(pattern, "#.##..##.")
+   pattern = append(pattern, "..#.##.#.")
+   pattern = append(pattern, "##......#")
+   pattern = append(pattern, "##......#")
+   pattern = append(pattern, "..#.##.#.")
+   pattern = append(pattern, "..##..##.")
+   pattern = append(pattern, "#.#.##.#.")
 
-	requiredNumSmudges := 0
+   requiredNumSmudges := 0
    reflectionIndex, _ := FindColReflectionIndex(pattern, requiredNumSmudges)
    expected := 4
 
@@ -46,7 +46,7 @@ func Test_FindRowReflectionIndex(t *testing.T) {
    pattern = append(pattern, "..##..###")
    pattern = append(pattern, "#....#..#")
 
-	requiredNumSmudges := 0
+   requiredNumSmudges := 0
    reflectionIndex, _ := FindRowReflectionIndex(pattern, requiredNumSmudges)
    expected := 3
 
@@ -62,43 +62,43 @@ func Test_FindRowReflectionIndex(t *testing.T) {
 //--------------------------------------------------------------------------------------------------
 func Test_GetSummaryValue(t *testing.T) {
 
-	var patterns [][]string
+   var patterns [][]string
 
-	{
-		var pattern []string
+   {
+      var pattern []string
 
-		pattern = append(pattern, "#.##..##.")
-		pattern = append(pattern, "..#.##.#.")
-		pattern = append(pattern, "##......#")
-		pattern = append(pattern, "##......#")
-		pattern = append(pattern, "..#.##.#.")
-		pattern = append(pattern, "..##..##.")
-		pattern = append(pattern, "#.#.##.#.")
+      pattern = append(pattern, "#.##..##.")
+      pattern = append(pattern, "..#.##.#.")
+      pattern = append(pattern, "##......#")
+      pattern = append(pattern, "##......#")
+      pattern = append(pattern, "..#.##.#.")
+      pattern = append(pattern, "..##..##.")
+      pattern = append(pattern, "#.#.##.#.")
 
-		patterns = append(patterns, pattern)
-	}
+      patterns = append(patterns, pattern)
+   }
 
-	{
-		var pattern []string
+   {
+      var pattern []string
 
-		pattern = append(pattern, "#...##..#")
-		pattern = append(pattern, "#....#..#")
-		pattern = append(pattern, "..##..###")
-		pattern = append(pattern, "#####.##.")
-		pattern = append(pattern, "#####.##.")
-		pattern = append(pattern, "..##..###")
-		pattern = append(pattern, "#....#..#")
+      pattern = append(pattern, "#...##..#")
+      pattern = append(pattern, "#....#..#")
+      pattern = append(pattern, "..##..###")
+      pattern = append(pattern, "#####.##.")
+      pattern = append(pattern, "#####.##.")
+      pattern = append(pattern, "..##..###")
+      pattern = append(pattern, "#....#..#")
 
-		patterns = append(patterns, pattern)
-	}
+      patterns = append(patterns, pattern)
+   }
 
-	requiredNumSmudges := 0
-	patternSummaryValue := GetSummaryValue(patterns, requiredNumSmudges)
+   requiredNumSmudges := 0
+   patternSummaryValue := GetSummaryValue(patterns, requiredNumSmudges)
 
-	if patternSummaryValue != 405 {
+   if patternSummaryValue != 405 {
       log.Println(patternSummaryValue)
-	   t.Fatal()
-	}
+      t.Fatal()
+   }
 }
 
 //--------------------------------------------------------------------------------------------------
@@ -108,15 +108,15 @@ func Test_FindRowReflectionIndex2_1(t *testing.T) {
 
    var pattern []string
 
-	pattern = append(pattern, "#.##..##.")
-	pattern = append(pattern, "..#.##.#.")
-	pattern = append(pattern, "##......#")
-	pattern = append(pattern, "##......#")
-	pattern = append(pattern, "..#.##.#.")
-	pattern = append(pattern, "..##..##.")
-	pattern = append(pattern, "#.#.##.#.")
+   pattern = append(pattern, "#.##..##.")
+   pattern = append(pattern, "..#.##.#.")
+   pattern = append(pattern, "##......#")
+   pattern = append(pattern, "##......#")
+   pattern = append(pattern, "..#.##.#.")
+   pattern = append(pattern, "..##..##.")
+   pattern = append(pattern, "#.#.##.#.")
 
-	requiredNumSmudges := 1
+   requiredNumSmudges := 1
    reflectionIndex, _ := FindRowReflectionIndex(pattern, requiredNumSmudges)
    expected := 2
 
@@ -142,7 +142,7 @@ func Test_FindRowReflectionIndex2_2(t *testing.T) {
    pattern = append(pattern, "..##..###")
    pattern = append(pattern, "#....#..#")
 
-	requiredNumSmudges := 1
+   requiredNumSmudges := 1
    reflectionIndex, _ := FindRowReflectionIndex(pattern, requiredNumSmudges)
    expected := 0
 
@@ -158,43 +158,43 @@ func Test_FindRowReflectionIndex2_2(t *testing.T) {
 //--------------------------------------------------------------------------------------------------
 func Test_GetSummaryValue2(t *testing.T) {
 
-	var patterns [][]string
+   var patterns [][]string
 
-	{
-		var pattern []string
+   {
+      var pattern []string
 
-		pattern = append(pattern, "#.##..##.")
-		pattern = append(pattern, "..#.##.#.")
-		pattern = append(pattern, "##......#")
-		pattern = append(pattern, "##......#")
-		pattern = append(pattern, "..#.##.#.")
-		pattern = append(pattern, "..##..##.")
-		pattern = append(pattern, "#.#.##.#.")
+      pattern = append(pattern, "#.##..##.")
+      pattern = append(pattern, "..#.##.#.")
+      pattern = append(pattern, "##......#")
+      pattern = append(pattern, "##......#")
+      pattern = append(pattern, "..#.##.#.")
+      pattern = append(pattern, "..##..##.")
+      pattern = append(pattern, "#.#.##.#.")
 
-		patterns = append(patterns, pattern)
-	}
+      patterns = append(patterns, pattern)
+   }
 
-	{
-		var pattern []string
+   {
+      var pattern []string
 
-		pattern = append(pattern, "#...##..#")
-		pattern = append(pattern, "#....#..#")
-		pattern = append(pattern, "..##..###")
-		pattern = append(pattern, "#####.##.")
-		pattern = append(pattern, "#####.##.")
-		pattern = append(pattern, "..##..###")
-		pattern = append(pattern, "#....#..#")
+      pattern = append(pattern, "#...##..#")
+      pattern = append(pattern, "#....#..#")
+      pattern = append(pattern, "..##..###")
+      pattern = append(pattern, "#####.##.")
+      pattern = append(pattern, "#####.##.")
+      pattern = append(pattern, "..##..###")
+      pattern = append(pattern, "#....#..#")
 
-		patterns = append(patterns, pattern)
-	}
+      patterns = append(patterns, pattern)
+   }
 
-	requiredNumSmudges := 1
-	patternSummaryValue := GetSummaryValue(patterns, requiredNumSmudges)
+   requiredNumSmudges := 1
+   patternSummaryValue := GetSummaryValue(patterns, requiredNumSmudges)
 
-	if patternSummaryValue != 400 {
+   if patternSummaryValue != 400 {
       log.Println(patternSummaryValue)
-	   t.Fatal()
-	}
+      t.Fatal()
+   }
 }
 
 //--------------------------------------------------------------------------------------------------
@@ -204,41 +204,41 @@ func Test_Transpose(t *testing.T) {
 
    var pattern []string
 
-	pattern = append(pattern, "#.##..##.")
-	pattern = append(pattern, "..#.##.#.")
-	pattern = append(pattern, "##......#")
-	pattern = append(pattern, "##......#")
-	pattern = append(pattern, "..#.##.#.")
-	pattern = append(pattern, "..##..##.")
-	pattern = append(pattern, "#.#.##.#.")
+   pattern = append(pattern, "#.##..##.")
+   pattern = append(pattern, "..#.##.#.")
+   pattern = append(pattern, "##......#")
+   pattern = append(pattern, "##......#")
+   pattern = append(pattern, "..#.##.#.")
+   pattern = append(pattern, "..##..##.")
+   pattern = append(pattern, "#.#.##.#.")
 
-	transposedPattern := Transpose(pattern)
+   transposedPattern := Transpose(pattern)
 
    var expectedTransposedPattern []string
 
-	expectedTransposedPattern = append(expectedTransposedPattern, "#.##..#")
-	expectedTransposedPattern = append(expectedTransposedPattern, "..##...")
-	expectedTransposedPattern = append(expectedTransposedPattern, "##..###")
-	expectedTransposedPattern = append(expectedTransposedPattern, "#....#.")
-	expectedTransposedPattern = append(expectedTransposedPattern, ".#..#.#")
-	expectedTransposedPattern = append(expectedTransposedPattern, ".#..#.#")
-	expectedTransposedPattern = append(expectedTransposedPattern, "#....#.")
-	expectedTransposedPattern = append(expectedTransposedPattern, "##..###")
-	expectedTransposedPattern = append(expectedTransposedPattern, "..##...")
+   expectedTransposedPattern = append(expectedTransposedPattern, "#.##..#")
+   expectedTransposedPattern = append(expectedTransposedPattern, "..##...")
+   expectedTransposedPattern = append(expectedTransposedPattern, "##..###")
+   expectedTransposedPattern = append(expectedTransposedPattern, "#....#.")
+   expectedTransposedPattern = append(expectedTransposedPattern, ".#..#.#")
+   expectedTransposedPattern = append(expectedTransposedPattern, ".#..#.#")
+   expectedTransposedPattern = append(expectedTransposedPattern, "#....#.")
+   expectedTransposedPattern = append(expectedTransposedPattern, "##..###")
+   expectedTransposedPattern = append(expectedTransposedPattern, "..##...")
 
-	if len(transposedPattern) != len(expectedTransposedPattern) {
+   if len(transposedPattern) != len(expectedTransposedPattern) {
       t.Fatal()
-	}
+   }
 
-	if len(transposedPattern[0]) != len(expectedTransposedPattern[0]) {
+   if len(transposedPattern[0]) != len(expectedTransposedPattern[0]) {
       t.Fatal()
-	}
+   }
 
-	for row := range len(transposedPattern) {
-	   for col := range len(transposedPattern[0]) {
-			if transposedPattern[row][col] != expectedTransposedPattern[row][col] {
-			   t.Fatal()
-			}
-	   }
-	}
+   for row := range len(transposedPattern) {
+      for col := range len(transposedPattern[0]) {
+         if transposedPattern[row][col] != expectedTransposedPattern[row][col] {
+            t.Fatal()
+         }
+      }
+   }
 }
