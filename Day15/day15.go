462d8dc62c3a5f1bee59dbd7f484110a33a381ff
tak5633 <35234375+tak5633@users.noreply.github.com>
2025-06-22 01:00:00 -0400

    main - Feat(Day15): Part 2

--- Day15/day15.go
+++ Day15/day15.go
@@ -1,16 +1,36 @@
 package main
 
 import (
-	"log"
-	"os"
-	"strings"
+   "log"
+   "os"
+   "strconv"
+   "strings"
+	"slices"
 )
+
+type StepFields struct {
+   mLabel string
+   mOperation rune
+   mHasFocalLength bool
+   mFocalLength int
+}
+
+type LenseInfo struct {
+   mLabel string
+   mFocalLength int
+}
+
+type Box struct {
+   mIndex int
+   mLenseInfoList []LenseInfo
+}
 
 //--------------------------------------------------------------------------------------------------
 //
 //--------------------------------------------------------------------------------------------------
 func main() {
    part1()
+   part2()
 }
 
 //--------------------------------------------------------------------------------------------------
@@ -22,8 +42,34 @@ func part1() {
 
    input := ReadInput()
 
-	hash := GetHash(input)
-	log.Println("Hash:", hash)
+   hash := GetHash(input)
+   log.Println("Hash:", hash)
+}
+
+//--------------------------------------------------------------------------------------------------
+//
+//--------------------------------------------------------------------------------------------------
+func part2() {
+
+   log.Println("Part 2")
+
+   input := ReadInput()
+
+   boxes := make([]Box, 256)
+
+   for i := range boxes {
+      boxes[i].mIndex = i
+   }
+
+   ProcessSteps(input, boxes)
+
+   focusingPower := 0
+
+   for _, box := range boxes {
+      focusingPower += box.CalculateFocusingPower()
+   }
+
+   log.Println("Focusing Power:", focusingPower)
 }
 
 //--------------------------------------------------------------------------------------------------
@@ -35,6 +81,34 @@ func ReadInput() string {
    check(err)
 
    return string(input)
+}
+
+//--------------------------------------------------------------------------------------------------
+//
+//--------------------------------------------------------------------------------------------------
+func ParseStep(pInput string) StepFields {
+   var stepFields StepFields
+
+   input := strings.TrimSpace(string(pInput))
+   fieldsSplit := strings.Split(input, "=")
+
+   if len(fieldsSplit) > 1 {
+      stepFields.mLabel = fieldsSplit[0]
+      stepFields.mOperation = '='
+      focalLength, err := strconv.ParseInt(fieldsSplit[1], 10, 0)
+      check(err)
+      stepFields.mHasFocalLength = true
+      stepFields.mFocalLength = int(focalLength)
+
+   } else {
+      fieldsSplit := strings.Split(pInput, "-")
+
+      stepFields.mLabel = fieldsSplit[0]
+      stepFields.mOperation = '-'
+      stepFields.mHasFocalLength = false
+   }
+
+   return stepFields
 }
 
 //--------------------------------------------------------------------------------------------------
@@ -52,40 +126,124 @@ func check(pE error) {
 //--------------------------------------------------------------------------------------------------
 func GetHash(pInput string) int {
 
-	hash := 0
+   hash := 0
    inputs := strings.Split(pInput, ",")
 
-	for _, input := range inputs {
-	   hash += GetStepHash(input)
-	}
+   for _, input := range inputs {
+      hash += GetStepHash(input)
+   }
 
-	return hash
+   return hash
 }
 
 //--------------------------------------------------------------------------------------------------
 //
 //--------------------------------------------------------------------------------------------------
 func GetStepHash(pInput string) int {
-	hash := 0
+   hash := 0
 
    for i := range pInput {
-		inputRune := rune(pInput[i])
+      inputRune := rune(pInput[i])
 
-		if inputRune != '\n' {
-			hash = GetNewHash(hash, inputRune)
-		}
+      if inputRune != '\n' {
+         hash = GetNewHash(hash, inputRune)
+      }
    }
 
-	return hash
+   return hash
 }
 
 //--------------------------------------------------------------------------------------------------
 //
 //--------------------------------------------------------------------------------------------------
 func GetNewHash(pHash int, pRune rune) int {
-	newHash := pHash + int(pRune)
-	newHash *= 17
-	newHash %= 256
+   newHash := pHash + int(pRune)
+   newHash *= 17
+   newHash %= 256
 
-	return newHash
+   return newHash
+}
+
+//--------------------------------------------------------------------------------------------------
+//
+//--------------------------------------------------------------------------------------------------
+func ProcessSteps(pInput string, pBoxes []Box) {
+
+   inputs := strings.Split(pInput, ",")
+
+   for _, input := range inputs {
+      stepFields := ParseStep(input)
+      ProcessStep(stepFields, pBoxes)
+   }
+}
+
+//--------------------------------------------------------------------------------------------------
+//
+//--------------------------------------------------------------------------------------------------
+func ProcessStep(pStepFields StepFields, pBoxes []Box) {
+
+   boxIndex := GetStepHash(pStepFields.mLabel)
+
+   if pStepFields.mOperation == '=' {
+      pBoxes[boxIndex].AddLense(pStepFields.mLabel, pStepFields.mFocalLength)
+
+   } else if pStepFields.mOperation == '-' {
+      pBoxes[boxIndex].RemoveLense(pStepFields.mLabel)
+   }
+}
+
+//--------------------------------------------------------------------------------------------------
+//
+//--------------------------------------------------------------------------------------------------
+func (box *Box) AddLense(pLabel string, pFocalLength int) {
+   for i := range box.mLenseInfoList {
+      if box.mLenseInfoList[i].mLabel == pLabel {
+         box.mLenseInfoList[i].mFocalLength = pFocalLength
+         return
+      }
+   }
+
+   lenseInfo := LenseInfo{}
+   lenseInfo.mLabel = pLabel
+   lenseInfo.mFocalLength = pFocalLength
+   box.mLenseInfoList = append(box.mLenseInfoList, lenseInfo)
+}
+
+//--------------------------------------------------------------------------------------------------
+//
+//--------------------------------------------------------------------------------------------------
+func (box *Box) RemoveLense(pLabel string) {
+   indexToRemove := -1
+
+   for i := range box.mLenseInfoList {
+      if box.mLenseInfoList[i].mLabel == pLabel {
+         indexToRemove = i
+      }
+   }
+
+   if indexToRemove == -1 {
+      return
+   }
+
+   box.mLenseInfoList = slices.Delete(box.mLenseInfoList, indexToRemove, indexToRemove+1)
+}
+
+//--------------------------------------------------------------------------------------------------
+//
+//--------------------------------------------------------------------------------------------------
+func (box *Box) CalculateFocusingPower() int {
+   focusingPower := 0
+
+   for lenseIndex, lenseInfo := range box.mLenseInfoList {
+      focusingPower += CalculateLenseFocusingPower(box.mIndex, lenseIndex, lenseInfo.mFocalLength)
+   }
+
+   return focusingPower
+}
+
+//--------------------------------------------------------------------------------------------------
+//
+//--------------------------------------------------------------------------------------------------
+func CalculateLenseFocusingPower(pBoxIndex int, pLenseIndex int, pFocalLength int) int {
+   return (pBoxIndex+1) * (pLenseIndex+1) * pFocalLength
 }
--- Day15/day15_test.go
+++ Day15/day15_test.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"log"
-	"testing"
+   "log"
+   "testing"
 )
 
 //--------------------------------------------------------------------------------------------------
@@ -10,146 +10,501 @@ import (
 //--------------------------------------------------------------------------------------------------
 func Test_GetStepHash(t *testing.T) {
 
-	{
-		input := "HASH"
-		hash := GetStepHash(input)
-		expected := 52
+   {
+      input := "HASH"
+      hash := GetStepHash(input)
+      expected := 52
 
-		if hash != expected {
-		   log.Println(hash)
-			log.Println(expected)
+      if hash != expected {
+         log.Println(hash)
+         log.Println(expected)
+         t.Fatal()
+      }
+   }
+
+   {
+      input := "rn=1"
+      hash := GetStepHash(input)
+      expected := 30
+
+      if hash != expected {
+         log.Println(hash)
+         log.Println(expected)
+         t.Fatal()
+      }
+   }
+
+   {
+      input := "cm-"
+      hash := GetStepHash(input)
+      expected := 253
+
+      if hash != expected {
+         log.Println(hash)
+         log.Println(expected)
+         t.Fatal()
+      }
+   }
+
+   {
+      input := "qp=3"
+      hash := GetStepHash(input)
+      expected := 97
+
+      if hash != expected {
+         log.Println(hash)
+         log.Println(expected)
+         t.Fatal()
+      }
+   }
+
+   {
+      input := "cm=2"
+      hash := GetStepHash(input)
+      expected := 47
+
+      if hash != expected {
+         log.Println(hash)
+         log.Println(expected)
+         t.Fatal()
+      }
+   }
+
+   {
+      input := "qp-"
+      hash := GetStepHash(input)
+      expected := 14
+
+      if hash != expected {
+         log.Println(hash)
+         log.Println(expected)
+         t.Fatal()
+      }
+   }
+
+   {
+      input := "pc=4"
+      hash := GetStepHash(input)
+      expected := 180
+
+      if hash != expected {
+         log.Println(hash)
+         log.Println(expected)
+         t.Fatal()
+      }
+   }
+
+   {
+      input := "ot=9"
+      hash := GetStepHash(input)
+      expected := 9
+
+      if hash != expected {
+         log.Println(hash)
+         log.Println(expected)
+         t.Fatal()
+      }
+   }
+
+   {
+      input := "ab=5"
+      hash := GetStepHash(input)
+      expected := 197
+
+      if hash != expected {
+         log.Println(hash)
+         log.Println(expected)
+         t.Fatal()
+      }
+   }
+
+   {
+      input := "pc-"
+      hash := GetStepHash(input)
+      expected := 48
+
+      if hash != expected {
+         log.Println(hash)
+         log.Println(expected)
+         t.Fatal()
+      }
+   }
+
+   {
+      input := "pc=6"
+      hash := GetStepHash(input)
+      expected := 214
+
+      if hash != expected {
+         log.Println(hash)
+         log.Println(expected)
+         t.Fatal()
+      }
+   }
+
+   {
+      input := "ot=7"
+      hash := GetStepHash(input)
+      expected := 231
+
+      if hash != expected {
+         log.Println(hash)
+         log.Println(expected)
+         t.Fatal()
+      }
+   }
+}
+
+//--------------------------------------------------------------------------------------------------
+//
+//--------------------------------------------------------------------------------------------------
+func Test_GetHash(t *testing.T) {
+
+   input := "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7"
+   hash := GetHash(input)
+   expected := 1320
+
+   if hash != expected {
+      log.Println(hash)
+      log.Println(expected)
+      t.Fatal()
+   }
+}
+
+//--------------------------------------------------------------------------------------------------
+//
+//--------------------------------------------------------------------------------------------------
+func AreStepFieldsEqual(pA StepFields, pB StepFields) bool {
+   return pA.mLabel == pB.mLabel && pA.mOperation == pB.mOperation && pA.mHasFocalLength == pB.mHasFocalLength && pA.mFocalLength == pB.mFocalLength
+}
+
+//--------------------------------------------------------------------------------------------------
+//
+//--------------------------------------------------------------------------------------------------
+func Test_ParseStep(t *testing.T) {
+
+   {
+      input := "rn=1"
+      stepFields := ParseStep(input)
+
+      var expected StepFields
+      expected.mLabel = "rn"
+      expected.mOperation = '='
+      expected.mHasFocalLength = true
+      expected.mFocalLength = 1
+
+      if !AreStepFieldsEqual(stepFields, expected) {
+         log.Println(stepFields)
+         log.Println(expected)
+         t.Fatal()
+      }
+   }
+
+   {
+      input := "cm-"
+      stepFields := ParseStep(input)
+
+      var expected StepFields
+      expected.mLabel = "cm"
+      expected.mOperation = '-'
+      expected.mHasFocalLength = false
+
+      if !AreStepFieldsEqual(stepFields, expected) {
+         log.Println(stepFields)
+         log.Println(expected)
+         t.Fatal()
+      }
+   }
+
+   {
+      input := "qp=3"
+      stepFields := ParseStep(input)
+
+      var expected StepFields
+      expected.mLabel = "qp"
+      expected.mOperation = '='
+      expected.mHasFocalLength = true
+      expected.mFocalLength = 3
+
+      if !AreStepFieldsEqual(stepFields, expected) {
+         log.Println(stepFields)
+         log.Println(expected)
+         t.Fatal()
+      }
+   }
+
+   {
+      input := "cm=2"
+      stepFields := ParseStep(input)
+
+      var expected StepFields
+      expected.mLabel = "cm"
+      expected.mOperation = '='
+      expected.mHasFocalLength = true
+      expected.mFocalLength = 2
+
+      if !AreStepFieldsEqual(stepFields, expected) {
+         log.Println(stepFields)
+         log.Println(expected)
+         t.Fatal()
+      }
+   }
+
+   {
+      input := "qp-"
+      stepFields := ParseStep(input)
+
+      var expected StepFields
+      expected.mLabel = "qp"
+      expected.mOperation = '-'
+      expected.mHasFocalLength = false
+
+      if !AreStepFieldsEqual(stepFields, expected) {
+         log.Println(stepFields)
+         log.Println(expected)
+         t.Fatal()
+      }
+   }
+
+   {
+      input := "pc=4"
+      stepFields := ParseStep(input)
+
+      var expected StepFields
+      expected.mLabel = "pc"
+      expected.mOperation = '='
+      expected.mHasFocalLength = true
+      expected.mFocalLength = 4
+
+      if !AreStepFieldsEqual(stepFields, expected) {
+         log.Println(stepFields)
+         log.Println(expected)
+         t.Fatal()
+      }
+   }
+
+   {
+      input := "ot=9"
+      stepFields := ParseStep(input)
+
+      var expected StepFields
+      expected.mLabel = "ot"
+      expected.mOperation = '='
+      expected.mHasFocalLength = true
+      expected.mFocalLength = 9
+
+      if !AreStepFieldsEqual(stepFields, expected) {
+         log.Println(stepFields)
+         log.Println(expected)
+         t.Fatal()
+      }
+   }
+
+   {
+      input := "ab=5"
+      stepFields := ParseStep(input)
+
+      var expected StepFields
+      expected.mLabel = "ab"
+      expected.mOperation = '='
+      expected.mHasFocalLength = true
+      expected.mFocalLength = 5
+
+      if !AreStepFieldsEqual(stepFields, expected) {
+         log.Println(stepFields)
+         log.Println(expected)
+         t.Fatal()
+      }
+   }
+
+   {
+      input := "pc-"
+      stepFields := ParseStep(input)
+
+      var expected StepFields
+      expected.mLabel = "pc"
+      expected.mOperation = '-'
+      expected.mHasFocalLength = false
+
+      if !AreStepFieldsEqual(stepFields, expected) {
+         log.Println(stepFields)
+         log.Println(expected)
+         t.Fatal()
+      }
+   }
+
+   {
+      input := "pc=6"
+      stepFields := ParseStep(input)
+
+      var expected StepFields
+      expected.mLabel = "pc"
+      expected.mOperation = '='
+      expected.mHasFocalLength = true
+      expected.mFocalLength = 6
+
+      if !AreStepFieldsEqual(stepFields, expected) {
+         log.Println(stepFields)
+         log.Println(expected)
+         t.Fatal()
+      }
+   }
+
+   {
+      input := "ot=7"
+      stepFields := ParseStep(input)
+
+      var expected StepFields
+      expected.mLabel = "ot"
+      expected.mOperation = '='
+      expected.mHasFocalLength = true
+      expected.mFocalLength = 7
+
+      if !AreStepFieldsEqual(stepFields, expected) {
+         log.Println(stepFields)
+         log.Println(expected)
+         t.Fatal()
+      }
+   }
+}
+
+//--------------------------------------------------------------------------------------------------
+//
+//--------------------------------------------------------------------------------------------------
+func Test_AddLense(t *testing.T) {
+	box := Box{}
+
+	{
+		label := "ot"
+		focalLength := 9
+		box.AddLense(label, focalLength)
+
+      if len(box.mLenseInfoList) != 1 ||
+		   box.mLenseInfoList[0].mLabel != label ||
+		   box.mLenseInfoList[0].mFocalLength != focalLength {
+
+			log.Println(box.mLenseInfoList[0])
+         t.Fatal()
+      }
+	}
+
+	{
+		label := "ab"
+		focalLength := 5
+		box.AddLense(label, focalLength)
+
+      if len(box.mLenseInfoList) != 2 ||
+		   box.mLenseInfoList[1].mLabel != label ||
+		   box.mLenseInfoList[1].mFocalLength != focalLength {
+
+			log.Println(box.mLenseInfoList[0])
+         t.Fatal()
+      }
+	}
+
+	{
+		label := "ot"
+		focalLength := 7
+		box.AddLense(label, focalLength)
+
+      if len(box.mLenseInfoList) != 2 ||
+		   box.mLenseInfoList[0].mLabel != label ||
+		   box.mLenseInfoList[0].mFocalLength != focalLength {
+
+			log.Println(box.mLenseInfoList[0])
+         t.Fatal()
+      }
+	}
+}
+
+//--------------------------------------------------------------------------------------------------
+//
+//--------------------------------------------------------------------------------------------------
+func Test_RemoveLense(t *testing.T) {
+
+	{
+		// Beginning
+		box := Box{}
+
+		box.AddLense("qp", 0)
+
+		if len(box.mLenseInfoList) != 1 ||
+		   box.mLenseInfoList[0].mLabel != "qp" {
+			t.Fatal()
+		}
+
+		box.RemoveLense("nomatch")
+
+		if len(box.mLenseInfoList) != 1 ||
+		   box.mLenseInfoList[0].mLabel != "qp" {
+			t.Fatal()
+		}
+
+		box.RemoveLense("qp")
+
+		if len(box.mLenseInfoList) != 0 {
 			t.Fatal()
 		}
 	}
 
 	{
-		input := "rn=1"
-		hash := GetStepHash(input)
-		expected := 30
+		// Middle
+		box := Box{}
 
-		if hash != expected {
-		   log.Println(hash)
-			log.Println(expected)
+		box.AddLense("kt", 0)
+		box.AddLense("qp", 0)
+		box.AddLense("ck", 0)
+
+		if len(box.mLenseInfoList) != 3 ||
+		   box.mLenseInfoList[0].mLabel != "kt" ||
+		   box.mLenseInfoList[1].mLabel != "qp" ||
+		   box.mLenseInfoList[2].mLabel != "ck" {
 			t.Fatal()
 		}
-	}
-	
-	{
-		input := "cm-"
-		hash := GetStepHash(input)
-		expected := 253
 
-		if hash != expected {
-		   log.Println(hash)
-			log.Println(expected)
+		box.RemoveLense("nomatch")
+
+		if len(box.mLenseInfoList) != 3 ||
+		   box.mLenseInfoList[0].mLabel != "kt" ||
+		   box.mLenseInfoList[1].mLabel != "qp" ||
+		   box.mLenseInfoList[2].mLabel != "ck" {
 			t.Fatal()
 		}
-	}
 
-	{
-		input := "qp=3"
-		hash := GetStepHash(input)
-		expected := 97
+		box.RemoveLense("qp")
 
-		if hash != expected {
-		   log.Println(hash)
-			log.Println(expected)
-			t.Fatal()
-		}
-	}
-
-	{
-		input := "cm=2"
-		hash := GetStepHash(input)
-		expected := 47
-
-		if hash != expected {
-		   log.Println(hash)
-			log.Println(expected)
+		if len(box.mLenseInfoList) != 2 ||
+		   box.mLenseInfoList[0].mLabel != "kt" ||
+		   box.mLenseInfoList[1].mLabel != "ck" {
 			t.Fatal()
 		}
 	}
 
 	{
-		input := "qp-"
-		hash := GetStepHash(input)
-		expected := 14
+		// End
+		box := Box{}
 
-		if hash != expected {
-		   log.Println(hash)
-			log.Println(expected)
+		box.AddLense("kt", 0)
+		box.AddLense("qp", 0)
+
+		if len(box.mLenseInfoList) != 2 ||
+		   box.mLenseInfoList[0].mLabel != "kt" ||
+		   box.mLenseInfoList[1].mLabel != "qp" {
 			t.Fatal()
 		}
-	}
 
-	{
-		input := "pc=4"
-		hash := GetStepHash(input)
-		expected := 180
+		box.RemoveLense("nomatch")
 
-		if hash != expected {
-		   log.Println(hash)
-			log.Println(expected)
+		if len(box.mLenseInfoList) != 2 ||
+		   box.mLenseInfoList[0].mLabel != "kt" ||
+		   box.mLenseInfoList[1].mLabel != "qp" {
 			t.Fatal()
 		}
-	}
 
-	{
-		input := "ot=9"
-		hash := GetStepHash(input)
-		expected := 9
+		box.RemoveLense("qp")
 
-		if hash != expected {
-		   log.Println(hash)
-			log.Println(expected)
-			t.Fatal()
-		}
-	}
-
-	{
-		input := "ab=5"
-		hash := GetStepHash(input)
-		expected := 197
-
-		if hash != expected {
-		   log.Println(hash)
-			log.Println(expected)
-			t.Fatal()
-		}
-	}
-
-	{
-		input := "pc-"
-		hash := GetStepHash(input)
-		expected := 48
-
-		if hash != expected {
-		   log.Println(hash)
-			log.Println(expected)
-			t.Fatal()
-		}
-	}
-
-	{
-		input := "pc=6"
-		hash := GetStepHash(input)
-		expected := 214
-
-		if hash != expected {
-		   log.Println(hash)
-			log.Println(expected)
-			t.Fatal()
-		}
-	}
-
-	{
-		input := "ot=7"
-		hash := GetStepHash(input)
-		expected := 231
-
-		if hash != expected {
-		   log.Println(hash)
-			log.Println(expected)
+		if len(box.mLenseInfoList) != 1 ||
+		   box.mLenseInfoList[0].mLabel != "kt" {
 			t.Fatal()
 		}
 	}
@@ -158,15 +513,63 @@ func Test_GetStepHash(t *testing.T) {
 //--------------------------------------------------------------------------------------------------
 //
 //--------------------------------------------------------------------------------------------------
-func Test_GetHash(t *testing.T) {
+func Test_CalculateFocusingPower(t *testing.T) {
 
-	input := "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7"
-	hash := GetHash(input)
-	expected := 1320
+	{
+		box := Box{}
+		box.mIndex = 0
 
-	if hash != expected {
-		log.Println(hash)
-		log.Println(expected)
-		t.Fatal()
+		{
+			var lenseInfo LenseInfo
+			lenseInfo.mFocalLength = 1
+			box.mLenseInfoList = append(box.mLenseInfoList, lenseInfo)
+		}
+
+		{
+			var lenseInfo LenseInfo
+			lenseInfo.mFocalLength = 2
+			box.mLenseInfoList = append(box.mLenseInfoList, lenseInfo)
+		}
+
+		focusingPower := box.CalculateFocusingPower()
+		expected := 5
+
+		if focusingPower != expected {
+			log.Println(focusingPower)
+			log.Println(expected)
+			t.Fatal()
+		}
+	}
+
+	{
+		box := Box{}
+		box.mIndex = 3
+
+		{
+			var lenseInfo LenseInfo
+			lenseInfo.mFocalLength = 7
+			box.mLenseInfoList = append(box.mLenseInfoList, lenseInfo)
+		}
+
+		{
+			var lenseInfo LenseInfo
+			lenseInfo.mFocalLength = 5
+			box.mLenseInfoList = append(box.mLenseInfoList, lenseInfo)
+		}
+
+		{
+			var lenseInfo LenseInfo
+			lenseInfo.mFocalLength = 6
+			box.mLenseInfoList = append(box.mLenseInfoList, lenseInfo)
+		}
+
+		focusingPower := box.CalculateFocusingPower()
+		expected := 140
+
+		if focusingPower != expected {
+			log.Println(focusingPower)
+			log.Println(expected)
+			t.Fatal()
+		}
 	}
 }
